0feff22e832aea87f375a5f9569d500bdaf30c48
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-03-11 09:11:30 +0530

    some changes made

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
